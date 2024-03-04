9292f2dfa1cd0a292d823c3cdec055c32f7e418c
ryucraftz <vaibhavsathe020>
2024-03-04 16:37:11 +0530

    3rd party r-based login added

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,28 @@
-import 'package:fire_track/loginscreen.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+import 'package:flutter/material.dart';
 
-void main() async {
+import 'register.dart';
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: 'AIzaSyC9jbQLOBCvg_9qnJwNOKQyvmA5xxlWnjI',
-      appId: '1:324321966537:android:b4938df33f352563e65018',
-      messagingSenderId: '324321966537',
-      projectId: 'fire-track-60af1',
-    ),
-  );
-  runApp(const MyApp());
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-  
-      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange.shade600),
-        useMaterial3: true,
+        primaryColor: Colors.blue[900],
       ),
-      home: const KeyboardVisibilityProvider(
-        child: LoginScreen(),
-      ),
+      home: Register(),
     );
   }
 }
-
